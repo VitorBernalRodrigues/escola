9ef99a51652ee0a950a2974db499d74f48d4e6f0
Vitor Hugo Rodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-10-14 21:36:39 -0400

    Update server.js

--- aulaservidorexpress/src/server.js
+++ aulaservidorexpress/src/server.js
@@ -1,15 +1,28 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from "express";
+
+const app = express();
+app.use(express.json())
+
+const usuarios = []
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send("Voce esta na pagina principal get")
 })
 
-app.post('/', (req, res) => {
-    res.send("Voce estas na pagina principal")
+app.get('/', (req, res) => {
+    res.status(200).json(usuarios)
+})
+
+app.post('/usuario', (req, res) => {
+    const {nome, idade} = req.body
+    usuarios.push({nome, idade})
+    console.log(usuarios)
+
+    res.status(201).send("Usuario Criado")
+
+    res.send("Voce esta na pagina principal post")
 }) 
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(3333, () => {
+    console.log("Servidor rodando na porta https://localhost:3333")
 })
