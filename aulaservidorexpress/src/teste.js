5b465154e296b60aa0bc31c5d57d65d577533665
VitorBernalRodrigues <125226491+VitorBernalRodrigues@users.noreply.github.com>
2025-10-15 16:20:26 -0400

    aula 8

---
+++ aulaservidorexpress/src/teste.js
@@ -0,0 +1,10 @@
+const banco = {
+    usuario: [
+        {nome: 'Pedro', idade : 17},
+        {nome: 'Henrique', idade: 18}
+    ],
+    produtos: [
+        {nome: "Mouse", valor: 150},
+        {nome: "teclado", valor: 200}
+    ]
+}
